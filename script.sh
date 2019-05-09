6ac28a979476524310d9a3fb2f29a18cb25b57e8
Daniel Alejandro Oliveros Duran <daoliverosd@unal.edu.co>
2019-05-08 23:38:08 -0500

    Update script.sh
    Remove BOM , First Query to get Month.

--- script.sh
+++ script.sh
@@ -15,23 +15,32 @@ do
     b=$(basename $i)
     filename="${b%.*}"
     echo $filename
-    sed 's#,#.#g ' $i > temporal/out2.csv
-    sed 's#;#,#g ' temporal/out2.csv > temporal/out3.csv
-    sed 's#/#-#g ' temporal/out3.csv > temporal/out4.csv
+    sed -i '1s/^\xEF\xBB\xBF//' $i
+    sed 's#,#.#g' $i > temporal/out2.csv
+    sed 's#;#,#g' temporal/out2.csv > temporal/out3.csv
+    sed 's#/#-#g' temporal/out3.csv > temporal/out4.csv
     sed 's#^\([0-9]\)-#0\1-#' temporal/out4.csv > temporal/out5.csv
     sed 's#\([0-9][0-9]\)#20\1#' temporal/out5.csv > temporal/out6.csv 
     sed "s#^\([0-9][0-9][0-9][0-9]\)#$filename,\1#" temporal/out6.csv > temporal/out7.csv
     sed "s#^\(FECHA\)#NAME,\1#" temporal/out7.csv > procesadoSed/$filename"Procesado.csv"
 done
-    rm -r temporal
+   rm -r temporal
 }
 
 JuntarArchivos(){
     echo 'hello, world'
 }
-ConsultaSQLYResultadoFinal(){
-    csvsql  -v --query "select DIR as Nombre,strftime('%m', FECHA) as Mes,AVG(VEL) as Velocidad from out6 GROUP BY  DIR" out6.csv > old.csv
+ConsultaSQL(){
+    mkdir ConsultaSQL
+    for i in ./procesadoSed/*.csv
+    do
+    b=$(basename $i)
+    filename="${b%.*}"
+    csvsql  -v --query "select NAME ,strftime('%m', FECHA) as Mes,AVG(VEL) as Velocidad from $filename  GROUP BY  NAME,MES"  $i > ConsultaSQL/$filename"mes.csv"
+    csvcut -n $i
+    done
 }
 
 
 AgregarFilasHaTodosLasEstaciones
+ConsultaSQL
